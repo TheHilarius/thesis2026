767d9942b2a983e46b448a7a51f03cb42c28726d
TheHilarius <s204581@student.dtu.dk>
2026-03-05 12:24:26 +0100

    fix error, so df_clean is saved

--- src/processing_of_healthy_and_cancer_data.R
+++ src/processing_of_healthy_and_cancer_data.R
@@ -11,7 +11,26 @@ colnames(df_raw) <- clean_names(colnames(df_raw))
 df_clean <- df_raw |>
   select(where(~ !all(is.na(.)))) |>
   mutate(across(where(is.character), ~ na_if(.x, ""))) |>
-  mutate(pep_length = nchar(epitope_name))
+  mutate(pep_length = nchar(epitope_name)) |>
+  select(c(
+    epitope_name,epitope_modifications, pep_length,
+    epitope_starting_position, epitope_ending_position,
+    epitope_source_molecule,epitope_molecule_parent, epitope_molecule_parent_iri,
+    in_vivo_process_process_type, in_vivo_process_disease)
+    ) |>
+  # remove epitope_ prefix from column names
+  rename_with(~ str_remove(.x, "^epitope_"), starts_with("epitope_")) |>
+  rename(
+    start = starting_position, end = ending_position,
+    peptide = name
+    ) |>
+  mutate(
+    uniprot_id = str_extract(molecule_parent_iri, "[^/]+$")) |>
+  select(-molecule_parent_iri) |>
+  distinct(peptide, uniprot_id, .keep_all = TRUE) |>
+  # remove modifications
+  filter(is.na(modifications)) |>
+  select(-modifications)
 
 df_healthy <- df_clean |>
   filter(in_vivo_process_disease=="healthy")
@@ -35,4 +54,10 @@ df_cancer <- df_clean |>
 
 write_csv(df_cancer, "data/pos_EL_cancer_epitopes_hla0201.csv")
 write_csv(df_healthy, "data/pos_EL_healthy_epitopes_hla0201.csv")
-write_csv(df_healthy, "data/pos_EL_all_epitopes_hla0201.csv")
+write_csv(df_clean, "data/pos_EL_all_epitopes_hla0201.csv")
+
+
+# Use src/export_fasta_files_from_iedb.py on data/pos_EL_all_epitopes_hla0201.csv
+
+
+  
